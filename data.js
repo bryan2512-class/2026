37c4cdf91bcb2f712b3501884197b75295aaea3d
BRYAN2512 <128509511+bryan2512-class@users.noreply.github.com>
2026-04-27 00:22:57 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,31 @@
+// data.js
+// 这是你的数据库，以后加题目只改这里
+const schoolData = [
+    {
+        name: "F4 ADDITIONAL MATHEMATICS",
+        units: [
+            {
+                name: "BAB 1: FUNGSI",
+                sections: [
+                    {
+                        name: "1.1 Fungsi (Chinese Version)",
+                        points: [
+                            {
+                                name: "Lesson 1: Pengenalan Fungsi",
+                                topics: [
+                                    { 
+                                        id: "f4_c1_q1", 
+                                        title: "Apakah Fungsi ?", 
+                                        quiz: "$$f\\colon x \\to y^2$$ <br> $$f(x) = y^2$$", 
+                                        quizImageId: "", 
+                                        driveId: ""
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+];
